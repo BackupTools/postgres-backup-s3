fa617136cf24009fcd53750db1c26f6f6a5bfeeb
Хориков Павел <pkhorikov@dtln.ru>
2021-02-10 13:53:29 +0300

    Remove usage of regular expressions, may be unstable

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,36 +21,11 @@ else
     gpg --keyserver ${GPG_KEYSERVER} --recv-keys ${GPG_KEYID}
 fi
 
-function uri_parser() {
-  # uri capture
-  uri="$@"
-
-  # safe escaping
-  uri="${uri//\`/%60}"
-  uri="${uri//\"/%22}"
-
-  # top level parsing
-  pattern='^(([a-z]{3,15})://)?((([^:\/]+)(:([^@\/]*))?@)?([^:\/?]+)(:([0-9]+))?)(\/[^?]*)?(\?[^#]*)?(#.*)?$'
-  [[ "$uri" =~ $pattern ]] || return 1;
-
-  # component extraction
-  uri=${BASH_REMATCH[0]}
-  uri_schema=${BASH_REMATCH[2]}
-  uri_address=${BASH_REMATCH[3]}
-  uri_user=${BASH_REMATCH[5]}
-  uri_password=${BASH_REMATCH[7]}
-  uri_host=${BASH_REMATCH[8]}
-  uri_port=${BASH_REMATCH[10]}
-  uri_path=${BASH_REMATCH[11]}
-}
-# uri_parser $PG_URI ; echo $? $uri_host $uri_user $uri_password $uri_port
-
 echo "$(get_date) Postgres backup started"
 
 export MC_HOST_backup=$S3_URI
 
 mc mb backup/${S3_BUCK} --insecure
-
 
 case $COMPRESS in
   'pigz' )
@@ -85,15 +60,13 @@ esac
 
 dump_db(){
   DATABASE=$1
-
   # Ping databaase
-  psql --host=${uri_host} --port=${uri_port} --username=${uri_user} --dbname=${DATABASE} -c ''
+  psql ${PG_URI%/}/${DATABASE} -c ''
 
   echo "$(get_date) Dumping database: $DATABASE"
+
   if [ -z "$GPG_KEYID" ]
   then
-    # true
-    # pg_dump ${PG_URI%/}/${DATABASE} | head -n 10
     pg_dump ${PG_URI%/}/${DATABASE} | $COMPRESS_CMD | mc pipe backup/${S3_BUCK}/${S3_NAME}-${START_DATE}-${DATABASE}.pgdump${COMPRESS_POSTFIX} --insecure
   else
     pg_dump ${PG_URI%/}/${DATABASE} | $COMPRESS_CMD \
@@ -108,13 +81,10 @@ then
   DB_NAME=""
 fi
 
-uri_parser $PG_URI
-export PGPASSWORD=${uri_password}
-
 if [ -z "$DB_NAME" ]
 then
   echo "$(get_date) No specific database selected. Saving each in separate files"
-  DB_LIST=$(psql --host=${uri_host} --port=${uri_port} --username=${uri_user} -A -c "SELECT datname FROM pg_database WHERE datname NOT LIKE 'template%';" | head -n -1 | tail -n +2)
+  DB_LIST=$(psql ${PG_URI%/} -A -c "SELECT datname FROM pg_database WHERE datname NOT LIKE 'template%';" | head -n -1 | tail -n +2)
   for db in $DB_LIST; do
     dump_db "$db"
   done
